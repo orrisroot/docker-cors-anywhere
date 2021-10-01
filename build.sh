2582505f93277d4ea83101bfae9095af6999a0ef
Yoshihiro OKUMURA <orrisroot@gmail.com>
2021-10-01 11:01:53 +0900

    append node version to image tag.

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+IMAGE=orrisroot/cors-anywhere
+REGISTORY=docker.io/${IMAGE}
 VERSION=0.4.4
 
-IMAGE=docker.io/orrisroot/cors-anywhere
 docker build --pull --force-rm -t ${IMAGE}:latest .
 IMAGE_ID=$(docker image ls orrisroot/cors-anywhere:latest -q)
+NODE_VERSION=$(docker run --rm -it ${IMAGE}:latest --version | sed -e "s/^v//" -e "s/\r$//")
 docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}
-docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}-$(date '+%Y%m%d')
+docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}-${NODE_VERSION}
 
 #docker login
-#docker image push --all-tags ${IMAGE}
+#docker image push --all-tags orrisroot/cors-anywhere
