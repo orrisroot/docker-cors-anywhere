95648ea8eb4d193500405ddb2c274483ac140ce6
Yoshihiro OKUMURA <orrisroot@gmail.com>
2022-11-16 14:31:30 +0900

    disable to assign node version to tag.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ docker build --pull --force-rm -t ${IMAGE}:latest .
 IMAGE_ID=$(docker image ls orrisroot/cors-anywhere:latest -q)
 NODE_VERSION=$(docker run --rm -it ${IMAGE}:latest --version | sed -e "s/^v//" -e "s/\r$//")
 docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}
-docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}-${NODE_VERSION}
+#docker image tag ${IMAGE_ID} ${IMAGE}:${VERSION}-${NODE_VERSION}
 
 #docker login
 echo Done! To push built images, run the following command.
