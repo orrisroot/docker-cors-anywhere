50f5b852976112aeb47ee12f29270fd978baba75
Yoshihiro OKUMURA <orrisroot@gmail.com>
2023-06-20 10:49:10 +0900

    support to check distroless image id.

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_IMAGE="gcr.io/distroless/nodejs:18"
+BASE_IMAGE="gcr.io/distroless/nodejs-debian11:18"
 TARGET_IMAGE="docker.io/orrisroot/cors-anywhere:latest"
 
 REQUIRED_COMMANDS=("skopeo" "jq" "basename")
@@ -15,18 +15,17 @@ done
 check_image () {
     local BASE_CONTAINER=$1
     local TARGET_CONTAINER=$2
-    local BASE_JSON=$(skopeo inspect docker://${BASE_CONTAINER})
+    local BASE_JSON=$(skopeo inspect --config docker://${BASE_CONTAINER})
     [ $? -eq 0 ] || return $?
-    local TARGET_JSON=$(skopeo inspect docker://${TARGET_CONTAINER})
+    local TARGET_JSON=$(skopeo inspect --config docker://${TARGET_CONTAINER})
     [ $? -eq 0 ] || return $?
-    local BASE_LAYER_ID=$(echo ${BASE_JSON} | jq "(.Layers | reverse)[0]")
-    local FOUND_LAYER_ID=$(echo ${TARGET_JSON} | jq ".Layers[] | select(. == ${BASE_LAYER_ID})")
+    local BASE_LAYER_ID=$(echo ${BASE_JSON} | jq "(.rootfs.diff_ids | reverse)[0]")
+    local FOUND_LAYER_ID=$(echo ${TARGET_JSON} | jq ".rootfs.diff_ids[] | select(. == ${BASE_LAYER_ID})")
 
     if [ "${BASE_LAYER_ID}" != "${FOUND_LAYER_ID}" ]; then
-        echo "The base Docker image has been updated."
-        echo "- ${BASE_CONTAINER}"
-        echo "The inherited Docker image needs to be updated."
+        echo "The Docker image needs to be updated."
         echo "- ${TARGET_CONTAINER}"
+        echo "  |- ${BASE_CONTAINER}  [newer version found]"
         echo ""
         return 1
     fi
